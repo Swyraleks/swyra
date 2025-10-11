d8701badd9309ab59724e40c4d98c399149be6d0
Aleksandar <aleksandarvlacic@hotmail.com>
2025-10-11 22:03:59 +0200

    Fix: correct relative imports in abg/page.tsx

--- app/abg/page.tsx
+++ app/abg/page.tsx
@@ -1,5 +1,6 @@
-import Features from "../../components/Features";
-import HowItWorks from "../../components/HowItWorks";
+// app/abg/page.tsx
+import Features from "../components/Features";
+import HowItWorks from "../components/HowItWorks";
 
 export default function Home() {
   return (
